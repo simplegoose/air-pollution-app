84beeb7377a06c08158c226074da7aa72d095c77
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-03-15 10:25:12 +0300

    - Import reducer into store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import cityReducer from './cities/citiesSlice';
 
-const store = configureStore();
+const store = configureStore({
+  reducer: {
+    cities: cityReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
 
 export default store;
