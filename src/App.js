e9062d530baeaf24d35f8da11f9bd295b1de3fe1
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-03-15 18:36:56 +0300

    - Route the pages available in the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Details from './routes/Details';
+import Home from './routes/Home';
+import './sass/index.scss';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/details/:name" element={<Details />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
