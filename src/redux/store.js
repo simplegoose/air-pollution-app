f57a6097d1085d535ef81f52090bef15d7504952
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-03-16 11:29:16 +0300

    - Add search reducer into the root reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import cityReducer from './cities/citiesSlice';
+import searchReducer from './search/searchSlice';
 
 const store = configureStore({
   reducer: {
     cities: cityReducer,
+    search: searchReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
