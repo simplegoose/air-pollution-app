315542d60aca73cfb0c51ae948f433e825eac9f1
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-03-16 09:53:42 +0300

    - Pass title to header by utilizing page route

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 
-const Layout = () => (
-  <>
-    <Header />
-    <Outlet />
-  </>
-);
+const Layout = () => {
+  const location = useLocation();
+
+  const title = location.pathname === '/' ? 'City view' : location.pathname.substring(9);
+
+  return (
+    <>
+      <Header title={title} />
+      <Outlet />
+    </>
+  );
+};
 
 export default Layout;
