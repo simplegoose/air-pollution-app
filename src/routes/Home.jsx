a040cd5aff5a8e099ef06f6ad32f795524133c38
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-03-16 11:55:15 +0300

    - Conditional render search component - Select isSearchShown from redux store - Select filtered cities to be rendered instead of cities

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,31 +2,45 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import hero from '../assets/img/air-pollution.png';
 import CityCard from '../components/CityCard';
+import Search from '../components/Search';
 
 const Home = () => {
-  const { cities: { cities } } = useSelector((state) => state);
+  const {
+    cities: {
+      filteredCities: cities,
+    },
+    search: {
+      isSearchOpen,
+    },
+  } = useSelector((state) => state);
 
   return (
-    <main className="home">
-      <section className="main-section">
-        <div className="hero">
-          <figure className="img-wrapper">
-            <img src={hero} alt="Pollution" className="hero-img" />
-          </figure>
-          <div className="title">
-            <h2>MAJOR CITIES</h2>
-            <h3>Air Pollution</h3>
+    <>
+      {
+        isSearchOpen
+        && <Search />
+      }
+      <main className="home">
+        <section className="main-section">
+          <div className="hero">
+            <figure className="img-wrapper">
+              <img src={hero} alt="Pollution" className="hero-img" />
+            </figure>
+            <div className="title">
+              <h2>MAJOR CITIES</h2>
+              <h3>Air Pollution</h3>
+            </div>
           </div>
-        </div>
-        <h4 className="stats-header">STATS BY CITY</h4>
-        <div className="cities">
-          {
-            cities
-            && cities.map((city) => <CityCard key={city.city} city={city} />)
-          }
-        </div>
-      </section>
-    </main>
+          <h4 className="stats-header">STATS BY CITY</h4>
+          <div className="cities">
+            {
+              cities
+              && cities.map((city) => <CityCard key={city.city} city={city} />)
+            }
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 
